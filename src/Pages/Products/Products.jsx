dbf677e2c739c2a37ac73baa34c9d2325e0f3719
programminghouse588 <thehouseofprogramming588@gmail.com>
2024-06-10 19:18:51 +0600

    implement chart

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -6,9 +6,13 @@ import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import useAccPro from "../../Hooks/useAccPro";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import toast from "react-hot-toast";
+import { useState } from "react";
 
 const Products = () => {
   const [acceptedProducts, , refetch] = useAccPro();
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 6;
+
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
   const navigate = useNavigate();
@@ -18,6 +22,16 @@ const Products = () => {
     mutationFn: async ({ id, userEmail }) =>
       await axiosSecure.put(`/voteCount/${id}`, { userEmail }),
   });
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = acceptedProducts?.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   const handleVoteCount = async (product) => {
     if (!user) {
       navigate("/login", { state: { from: location } });
@@ -36,68 +50,101 @@ const Products = () => {
     }
   };
   return (
-    <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-      {acceptedProducts?.map((product) => (
-        <div
-          key={product._id}
-          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
-        >
-          <img
-            src={product.productImage}
-            alt=""
-            className="h-80 w-72 object-cover rounded-t-xl"
-          />
-          <div className="px-4 py-3 w-72">
-            <div className="flex flex-wrap h-16 items-center">
-              {Array.isArray(product?.tags) &&
-                product?.tags?.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="mr-2 mb-2 bg-green-100 text-green-600 py-1 px-2 rounded-full text-sm font-semibold"
-                  >
-                    {tag}
-                  </span>
-                ))}
-            </div>
-            <NavLink to={`/details/${product._id}`}>
-              <p className="text-2xl mt-1 hover:underline font-bold text-black">
-                {product.productName}
-              </p>
-            </NavLink>
-            <p className="text-sm mt-4 h-32 font-semibold text-gray-500 italic">
-              {product.description}
-            </p>
-            <button className="px-4 mt-2 bg-green-500 italic text-base text-white font-semibold rounded-xl py-1">
-              {product.ProductStatus}
-            </button>
-
-            <div className="flex gap-2 mt-4 mb-3 justify-end">
-              <button
-                onClick={() => {
-                  if (product.voters?.includes(user?.email)) {
-                    toast.error("You've already voted this product");
-                  } else {
-                    handleVoteCount(product);
-                  }
-                }}
-                disabled={user?.email === product?.email}
-                className={`py-1 px-4 hover:text-green-600 hover:scale-105 hover:shadow text-center border rounded-md border-gray-300 h-8 text-sm flex items-center gap-1 lg:gap-2 ${
-                  user?.email === product?.email
-                    ? "cursor-not-allowed opacity-60 hover:text-black"
-                    : ""
-                }`}
-              >
-                <BiUpvote className="text-xl"></BiUpvote>
-                <span className="text-lg">{product?.upVote || 0}</span>
-              </button>
-              <button className="py-1 px-4 hover:text-red-600 hover:scale-105 hover:shadow text-center border border-gray-300 rounded-md h-8 text-sm flex items-center gap-1 lg:gap-2">
-                <BiDownvote className="hover:text-red-600 text-xl"></BiDownvote>
-                <span className="text-lg">0</span>
-              </button>
+    <div>
+      {/* Search */}
+      <div className="mt-14 mb-8">
+        <div className="container  flex justify-center items-center px-4 sm:px-6 lg:px-8">
+          <div className="relative">
+            <input
+              type="text"
+              className="h-14 w-96 pr-8 pl-5 border rounded z-0 shadow-2xl focus:outline-none"
+              placeholder="Search anything..."
+            />
+            <div className="absolute top-4 right-3">
+              <i className="fa fa-search text-gray-800 z-20 hover:text-gray-500" />
             </div>
           </div>
         </div>
-      ))}
+      </div>
+      <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        {currentProducts?.map((product) => (
+          <div
+            key={product._id}
+            className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+          >
+            <img
+              src={product.productImage}
+              alt=""
+              className="h-80 w-72 object-cover rounded-t-xl"
+            />
+            <div className="px-4 py-3 w-72">
+              <div className="flex flex-wrap h-16 items-center">
+                {Array.isArray(product?.tags) &&
+                  product?.tags?.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="mr-2 mb-2 bg-green-100 text-green-600 py-1 px-2 rounded-full text-sm font-semibold"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+              </div>
+              <NavLink to={`/details/${product._id}`}>
+                <p className="text-2xl mt-1 hover:underline font-bold text-black">
+                  {product.productName}
+                </p>
+              </NavLink>
+              <p className="text-sm mt-4 h-32 font-semibold text-gray-500 italic">
+                {product.description}
+              </p>
+              <button className="px-4 mt-2 bg-green-500 italic text-base text-white font-semibold rounded-xl py-1">
+                {product.ProductStatus}
+              </button>
+
+              <div className="flex gap-2 mt-4 mb-3 justify-end">
+                <button
+                  onClick={() => {
+                    if (product.voters?.includes(user?.email)) {
+                      toast.error("You've already voted this product");
+                    } else {
+                      handleVoteCount(product);
+                    }
+                  }}
+                  disabled={user?.email === product?.email}
+                  className={`py-1 px-4 hover:text-green-600 hover:scale-105 hover:shadow text-center border rounded-md border-gray-300 h-8 text-sm flex items-center gap-1 lg:gap-2 ${
+                    user?.email === product?.email
+                      ? "cursor-not-allowed opacity-60 hover:text-black"
+                      : ""
+                  }`}
+                >
+                  <BiUpvote className="text-xl"></BiUpvote>
+                  <span className="text-lg">{product?.upVote || 0}</span>
+                </button>
+                <button className="py-1 px-4 hover:text-red-600 hover:scale-105 hover:shadow text-center border border-gray-300 rounded-md h-8 text-sm flex items-center gap-1 lg:gap-2">
+                  <BiDownvote className="hover:text-red-600 text-xl"></BiDownvote>
+                  <span className="text-lg">0</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* Pagination */}
+      <div className="flex justify-center mt-14">
+        {Array.from({
+          length: Math.ceil(acceptedProducts.length / productsPerPage),
+        }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => paginate(index + 1)}
+            className={`px-6 py-2 text-3xl mx-2 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none ${
+              currentPage === index + 1 ? "bg-gray-500" : ""
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
